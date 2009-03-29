18d2f176f2668779eaf8cb7766c3a8db7df08e38
awostenberg <awostenberg@f0a95276-1093-11de-b0d4-7fec019aa467>
2009-03-29 08:08:30 +0000

    add unit tests for construction, erosion, and dilation

---
+++ trunk/TestImage.h
@@ -0,0 +1,15 @@
+//
+//  TestImage.h
+//
+//  Created by Alan Wostenberg on 3/26/09.
+//  Copyright 2009 Wosterware.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestImage : SenTestCase {
+
+}
+
+@end
---
+++ trunk/TestImage.mm
@@ -0,0 +1,110 @@
+//
+//  TestImage.mm
+//
+//  Created by Alan Wostenberg on 3/26/09.
+//  Copyright 2009 Wosterware.com. All rights reserved.
+//
+// To add OCUnit on which this depends to an Xcode project, 
+// see http://www.sente.ch/s/?p=535&lang=en and http://developer.apple.com/tools/unittest.html
+
+#import "TestImage.h"
+// the image.h is a mix of Objective C and C++
+#import "Image.h"
+
+@implementation TestImage
+
+// constructors
+
+- (void)testEmptyImageDimensions {
+	ImageWrapper* im = Image::createImage(10,20);
+
+	STAssertEquals(10, im.image->getWidth(), @"");
+	STAssertEquals(20, im.image->getHeight(), @"");
+}
+
+- (void)testFromBytes {
+	uint8_t imData[6] = {1,2,
+						3,4, 
+						5,6};
+	ImageWrapper* im = Image::createImage(imData,2,3);
+	
+	STAssertEquals(2, im.image->getWidth(), @"");
+	STAssertEquals(3, im.image->getHeight(), @"");
+	STAssertTrue(im.image->atRow(0) == imData, @"");	
+	//STAssertEquals(im.image->rowAt(0), imData, @"");
+	STAssertEquals(im.image->atXY(0,0), 1, @"");
+	STAssertEquals(im.image->atXY(1,0), 2, @"");
+	STAssertEquals(im.image->atXY(0,1), 3, @"");
+}
+
+
+// greyscale conversion from a tiny image
+
+- (void)testFromUIImage {
+	// The image is of uniform color who's value is encoded in the filename.
+	// The name r50g100b200 means red channel is 50, green is 100, blue is 200.
+	UIImage* uim = [UIImage imageWithContentsOfFile: @"r50g100b200.png"];
+	
+	ImageWrapper* im = Image::createImage(uim,uim.size.width,uim.size.height);
+	STAssertEquals(im.image->getWidth(),30,@"");
+	STAssertEquals(im.image->getHeight(),40,@"");
+	
+	//default uses green channel, which in the sample image, is 100
+	STAssertEquals(im.image->atXY(1,1), 100, @""); 
+
+	//all channels averaged to grey
+	im = Image::createImage(uim,uim.size.width,uim.size.height,false,Image::kRed|Image::kGreen|Image::kBlue);
+	STAssertEquals(im.image->atXY(1,1),  (50 + 100 + 200) / 3, @""); 
+
+	//red-green averaged to grey
+	im = Image::createImage(uim,uim.size.width,uim.size.height,false,Image::kRed|Image::kGreen);
+	STAssertEquals(im.image->atXY(1,1), (50+100)/2, @""); 
+}
+
+
+// inverting
+- (void)testInvert {
+	uint8_t imData[2] = {0, 255};
+	ImageWrapper* im = Image::createImage(imData,2,1);
+	im.image->invert();
+	STAssertEquals(im.image->atXY(0,0), 255, @"");
+	STAssertEquals(im.image->atXY(1,0), 0, @"");
+}
+
+// binary erosion
+// Sample data uses ranges of numbers for test readability. Real world values are 0 or 255.
+- (void)testErodeKeepsCentralPixel {
+	uint8_t imData[9] = { 1,2,3, 4,5,6, 7,8,9};
+	ImageWrapper* a = Image::createImage(imData,3,3);
+	
+	ImageWrapper* b = a.image->erode();
+	STAssertEquals(b.image->atXY(0,0), 1, @"");
+	STAssertEquals(b.image->atXY(1,1), 5, @"");
+	STAssertEquals(b.image->atXY(2,2), 9, @"");
+}
+
+- (void)testErodeZerosCentralPixel {
+	uint8_t imData[9] = {0,2,3, 4,5,6, 7,8,9};
+	ImageWrapper* a = Image::createImage(imData,3,3);
+	ImageWrapper* b = a.image->erode();
+	STAssertEquals(b.image->atXY(0,0), 0, @"");
+	STAssertEquals(b.image->atXY(1,1), 0, @"");
+	STAssertEquals(b.image->atXY(2,2), 9, @"");
+	
+	STAssertEquals(a.image->atXY(1,1), 5, @"");
+}
+
+// binary dilation
+- (void)testDilate {
+	uint8_t imData[9] = {1,0,0, 0,0,0, 0,0,0};
+	ImageWrapper* a = Image::createImage(imData,3,3);
+	
+	ImageWrapper* b = a.image->dilate();
+	STAssertEquals(b.image->atXY(0,0), 1, @"");
+	STAssertEquals(b.image->atXY(1,1), 255, @"");
+	STAssertEquals(b.image->atXY(2,2), 0, @"");
+}
+
+
+
+@end
