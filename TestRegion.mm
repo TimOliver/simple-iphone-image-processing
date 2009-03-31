a6ae2c16c91e62c7833722c72e29db643ccbd836
awostenberg <awostenberg@f0a95276-1093-11de-b0d4-7fec019aa467>
2009-03-31 20:50:19 +0000

    fix a bug with region bounding box swapping height and width calculations

--- TestRegion.mm
+++ TestRegion.mm
@@ -18,6 +18,8 @@
 	STAssertEquals(0,[r area], @"");
 	int w = r.bb.size.width;
 	STAssertEquals(0,w,@"");
+	int h = r.bb.size.height;
+	STAssertEquals(0,h,@"");
 }
 
 
@@ -35,14 +37,29 @@
 	[r addPoint: NSMakePoint(2,3)];
 	[r addPoint: NSMakePoint(5,5)];
 
-	int x = [r bb].origin.x;
+	int x = r.bb.origin.x;
 	STAssertEquals(2,x,@"");
-	int y = [r bb].origin.y;
+	int y = r.bb.origin.y;
 	STAssertEquals(3,y,@"");
 
 	STAssertEquals(200,[r area],@"");
 
 
+}
+
+- (void)testSmallRegionOutlier {
+	Region* r = [Region new];
+	[r addPoint: NSMakePoint(12,23)];
+	[r addPoint: NSMakePoint(100,5)];
+	[r addPoint: NSMakePoint(5,3)];
+	
+	int w = r.bb.size.width;
+	STAssertEquals(100-5,w,@"");
+	int h = r.bb.size.height;
+	STAssertEquals(23-3,h,@"");
+
+	
+	
 }
 
 @end
